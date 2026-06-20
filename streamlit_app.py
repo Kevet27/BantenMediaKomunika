939a86d9f291782cfe6f2dbc0cb8ea8544d16076
Kevet27 <kevetjunior@gmail.com>
2026-06-20 23:27:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,721 +1,151 @@
-import streamlit as st
 import sqlite3
-import hashlib
-import os
 
-# ================= CONFIG =================
+DB_NAME = "kasir.db"
 
-st.set_page_config(
-    page_title="TILAS BUT RAOS",
-    layout="wide"
-)
+def get_connection():
+    conn = sqlite3.connect(DB_NAME, check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
 
-# ================= FOLDER GAMBAR =================
 
-if not os.path.exists("gambar_produk"):
-    os.makedirs("gambar_produk")
+conn = get_connection()
+cur = conn.cursor()
 
-# ================= DATABASE =================
-
-conn = sqlite3.connect(
-    "tilas_raos.db",
-    check_same_thread=False
-)
-
-c = conn.cursor()
-
-# ================= TABEL =================
-
-c.execute("""
+# tabel user
+cur.execute("""
 CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT UNIQUE,
-password TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT,
+    role TEXT DEFAULT 'user'
 )
 """)
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS produk(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-nama TEXT,
-harga INTEGER,
-gambar TEXT
+# tabel barang
+cur.execute("""
+CREATE TABLE IF NOT EXISTS products(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    nama_barang TEXT,
+    harga INTEGER,
+    stok INTEGER
 )
 """)
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS keranjang(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user TEXT,
-produk TEXT,
-harga INTEGER
+# tabel transaksi
+cur.execute("""
+CREATE TABLE IF NOT EXISTS transactions(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total INTEGER
 )
 """)
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS pesanan(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user TEXT,
-alamat TEXT,
-pembayaran TEXT,
-total INTEGER
+# detail transaksi
+cur.execute("""
+CREATE TABLE IF NOT EXISTS transaction_detail(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    transaction_id INTEGER,
+    product_id INTEGER,
+    qty INTEGER,
+    subtotal INTEGER
 )
 """)
 
 conn.commit()
 
-# ================= SESSION =================
+# admin default
+try:
+    cur.execute(
+        "INSERT INTO users(username,password,role) VALUES(?,?,?)",
+        ("admin","admin123","admin")
+    )
+    conn.commit()
+except:
+    pass
+
+import streamlit as st
+from database import get_connection
+
+conn = get_connection()
+cur = conn.cursor()
+
+st.set_page_config(
+    page_title="Kasir UMKM",
+    layout="wide"
+)
 
 if "login" not in st.session_state:
     st.session_state.login = False
 
-if "user" not in st.session_state:
-    st.session_state.user = ""
+if "user_id" not in st.session_state:
+    st.session_state.user_id = None
 
-# ================= HASH PASSWORD =================
-
-def hash_password(password):
-
-    return hashlib.sha256(
-        password.encode()
-    ).hexdigest()
+if "role" not in st.session_state:
+    st.session_state.role = ""
 
 
-# ================= LOGIN =================
+def login():
+    st.title("LOGIN KASIR UMKM")
 
-def login_page():
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-    st.title("👕 TILAS BUT RAOS")
-
-    menu = st.selectbox(
-        "Menu",
-        ["Login","Register"]
-    )
-
-    if menu == "Register":
-
-        username = st.text_input(
-            "Username"
+    if st.button("Login"):
+        cur.execute(
+            "SELECT * FROM users WHERE username=? AND password=?",
+            (username, password)
         )
 
-        password = st.text_input(
-            "Password",
-            type="password"
-        )
+        user = cur.fetchone()
 
-        if st.button("Daftar"):
+        if user:
+            st.session_state.login = True
+            st.session_state.user_id = user["id"]
+            st.session_state.role = user["role"]
+            st.success("Login berhasil")
+            st.rerun()
+        else:
+            st.error("Username atau password salah")
 
-            try:
 
-                c.execute(
-                """
-                INSERT INTO users
-                (username,password)
-                VALUES(?,?)
-                """,
-                (
-                    username,
-                    hash_password(password)
-                )
-                )
+def register():
+    st.subheader("Daftar Akun")
 
-                conn.commit()
+    user = st.text_input("Username Baru")
+    pw = st.text_input("Password Baru", type="password")
 
-                st.success(
-                    "Akun berhasil dibuat"
-                )
-
-            except:
-
-                st.error(
-                    "Username sudah digunakan"
-                )
-
-    else:
-
-        username = st.text_input(
-            "Username"
-        )
-
-        password = st.text_input(
-            "Password",
-            type="password"
-        )
-
-        if st.button("Login"):
-
-            c.execute(
-            """
-            SELECT *
-            FROM users
-            WHERE username=?
-            AND password=?
-            """,
-            (
-                username,
-                hash_password(password)
+    if st.button("Daftar"):
+        try:
+            cur.execute(
+                "INSERT INTO users(username,password) VALUES(?,?)",
+                (user,pw)
             )
-            )
+            conn.commit()
+            st.success("Akun berhasil dibuat")
+        except:
+            st.error("Username sudah digunakan")
 
-            data = c.fetchone()
 
-            if data:
-
-                st.session_state.login = True
-                st.session_state.user = username
-
-                st.rerun()
-
-            else:
-
-                st.error(
-                    "Username atau password salah"
-                )
-
-# ================= BERANDA =================
-
-def home():
-
-    st.title("🏠 Beranda")
-
-    data = c.execute(
-        "SELECT * FROM produk"
-    ).fetchall()
-
-    if len(data) == 0:
-
-        st.info(
-            "Belum ada produk"
-        )
-
-    else:
-
-        col = st.columns(3)
-
-        nomor = 0
-
-        for p in data:
-
-            with col[nomor % 3]:
-
-                if p[3] != "":
-
-                    try:
-
-                        st.image(
-                            "gambar_produk/" + p[3],
-                            use_container_width=True
-                        )
-
-                    except:
-                        pass
-
-                st.subheader(
-                    p[1]
-                )
-
-                st.write(
-                    "Rp",
-                    format(
-                        p[2],
-                        ","
-                    )
-                )
-
-                if st.button(
-                    "Tambah Keranjang",
-                    key="produk"+str(p[0])
-                ):
-
-                    c.execute(
-                    """
-                    INSERT INTO keranjang
-                    (user,produk,harga)
-                    VALUES(?,?,?)
-                    """,
-                    (
-                        st.session_state.user,
-                        p[1],
-                        p[2]
-                    )
-                    )
-
-                    conn.commit()
-
-                    st.success(
-                        "Produk ditambahkan"
-                    )
-
-            nomor += 1
-
-
-# ================= KERANJANG =================
-
-def cart():
-
-    st.title("🛒 Keranjang")
-
-    data = c.execute(
-    """
-    SELECT *
-    FROM keranjang
-    WHERE user=?
-    """,
-    (
-        st.session_state.user,
-    )
-    ).fetchall()
-
-    total = 0
-
-    if len(data) == 0:
-
-        st.info(
-            "Keranjang kosong"
-        )
-
-    else:
-
-        for x in data:
-
-            col1, col2 = st.columns([4,1])
-
-            with col1:
-
-                st.write(
-                    x[2],
-                    "- Rp",
-                    format(
-                        x[3],
-                        ","
-                    )
-                )
-
-            with col2:
-
-                if st.button(
-                    "Hapus",
-                    key="hapus"+str(x[0])
-                ):
-
-                    c.execute(
-                    """
-                    DELETE FROM keranjang
-                    WHERE id=?
-                    """,
-                    (
-                        x[0],
-                    )
-                    )
-
-                    conn.commit()
-
-                    st.rerun()
-
-            total += x[3]
-
-        st.subheader(
-            "Total : Rp "
-            +
-            format(
-                total,
-                ","
-            )
-        )
-
-
-# ================= CHECKOUT =================
-
-def checkout():
-
-    st.title("📦 Checkout")
-
-    total = 0
-
-    data = c.execute(
-    """
-    SELECT harga
-    FROM keranjang
-    WHERE user=?
-    """,
-    (
-        st.session_state.user,
-    )
-    ).fetchall()
-
-    for x in data:
-
-        total += x[0]
-
-    st.subheader(
-        "Total Belanja : Rp "
-        +
-        format(
-            total,
-            ","
-        )
-    )
-
-    alamat = st.text_area(
-        "Alamat Lengkap"
-    )
-
-    pembayaran = st.selectbox(
-        "Metode Pembayaran",
-        [
-            "Transfer Bank",
-            "DANA",
-            "OVO"
-        ]
-    )
-
-    if st.button(
-        "Bayar"
-    ):
-
-        c.execute(
-        """
-        INSERT INTO pesanan
-        (user,alamat,pembayaran,total)
-        VALUES(?,?,?,?)
-        """,
-        (
-            st.session_state.user,
-            alamat,
-            pembayaran,
-            total
-        )
-        )
-
-        conn.commit()
-
-        c.execute(
-        """
-        DELETE FROM keranjang
-        WHERE user=?
-        """,
-        (
-            st.session_state.user,
-        )
-        )
-
-        conn.commit()
-
-        st.success(
-            "Pesanan berhasil dibuat"
-        )
-
-
-# ================= RIWAYAT PESANAN =================
-
-def riwayat():
-
-    st.title(
-        "📋 Riwayat Pesanan"
-    )
-
-    data = c.execute(
-    """
-    SELECT *
-    FROM pesanan
-    WHERE user=?
-    """,
-    (
-        st.session_state.user,
-    )
-    ).fetchall()
-
-    if len(data) == 0:
-
-        st.info(
-            "Belum ada pesanan"
-        )
-
-    else:
-
-        for x in data:
-
-            st.write(
-                "Alamat :",
-                x[2]
-            )
-
-            st.write(
-                "Pembayaran :",
-                x[3]
-            )
-
-            st.write(
-                "Total : Rp",
-                format(
-                    x[4],
-                    ","
-                )
-            )
-
-            st.divider()
-# ================= ADMIN PRODUK =================
-
-def admin_produk():
-
-    st.title("⚙️ Admin Produk")
-
-    tab1, tab2 = st.tabs(
-        ["Tambah Produk", "Kelola Produk"]
-    )
-
-    # ================= TAMBAH PRODUK =================
+if st.session_state.login == False:
+    tab1, tab2 = st.tabs(["Login","Register"])
 
     with tab1:
-
-        nama = st.text_input(
-            "Nama Produk"
-        )
-
-        harga = st.number_input(
-            "Harga",
-            min_value=1000,
-            step=1000
-        )
-
-        gambar = st.file_uploader(
-            "Upload Foto Produk",
-            type=["jpg","jpeg","png"]
-        )
-
-        if st.button(
-            "Tambah Produk"
-        ):
-
-            nama_file = ""
-
-            if gambar is not None:
-
-                nama_file = gambar.name
-
-                with open(
-                    os.path.join(
-                        "gambar_produk",
-                        nama_file
-                    ),
-                    "wb"
-                ) as f:
-
-                    f.write(
-                        gambar.getbuffer()
-                    )
-
-            c.execute(
-            """
-            INSERT INTO produk
-            (nama,harga,gambar)
-            VALUES(?,?,?)
-            """,
-            (
-                nama,
-                int(harga),
-                nama_file
-            )
-            )
-
-            conn.commit()
-
-            st.success(
-                "Produk berhasil ditambahkan"
-            )
-
-            st.rerun()
-
-    # ================= KELOLA PRODUK =================
+        login()
 
     with tab2:
-
-        data = c.execute(
-        """
-        SELECT *
-        FROM produk
-        """
-        ).fetchall()
-
-        if len(data) == 0:
-
-            st.info(
-                "Belum ada produk"
-            )
-
-        else:
-
-            for p in data:
-
-                st.divider()
-
-                col1, col2 = st.columns([1,2])
-
-                with col1:
-
-                    if p[3] != "":
-
-                        try:
-
-                            st.image(
-                                "gambar_produk/" + p[3],
-                                width=150
-                            )
-
-                        except:
-                            pass
-
-                with col2:
-
-                    nama_baru = st.text_input(
-                        "Nama Produk",
-                        value=p[1],
-                        key="nama"+str(p[0])
-                    )
-
-                    harga_baru = st.number_input(
-                        "Harga",
-                        value=int(p[2]),
-                        key="harga"+str(p[0])
-                    )
-
-                    gambar_baru = st.file_uploader(
-                        "Ganti Foto",
-                        type=["jpg","jpeg","png"],
-                        key="gambar"+str(p[0])
-                    )
-
-                    col_update, col_hapus = st.columns(2)
-
-                    with col_update:
-
-                        if st.button(
-                            "Update",
-                            key="update"+str(p[0])
-                        ):
-
-                            nama_file = p[3]
-
-                            if gambar_baru is not None:
-
-                                nama_file = gambar_baru.name
-
-                                with open(
-                                    os.path.join(
-                                        "gambar_produk",
-                                        nama_file
-                                    ),
-                                    "wb"
-                                ) as f:
-
-                                    f.write(
-                                        gambar_baru.getbuffer()
-                                    )
-
-                            c.execute(
-                            """
-                            UPDATE produk
-                            SET nama=?,
-                            harga=?,
-                            gambar=?
-                            WHERE id=?
-                            """,
-                            (
-                                nama_baru,
-                                int(harga_baru),
-                                nama_file,
-                                p[0]
-                            )
-                            )
-
-                            conn.commit()
-
-                            st.success(
-                                "Produk berhasil diperbarui"
-                            )
-
-                            st.rerun()
-
-                    with col_hapus:
-
-                        if st.button(
-                            "Hapus",
-                            key="hapus"+str(p[0])
-                        ):
-
-                            c.execute(
-                            """
-                            DELETE FROM produk
-                            WHERE id=?
-                            """,
-                            (
-                                p[0],
-                            )
-                            )
-
-                            conn.commit()
-
-                            st.rerun()
-
-
-# ================= MENU UTAMA =================
-
-if not st.session_state.login:
-
-    login_page()
+        register()
 
 else:
+    st.title("Kasir UMKM")
+    st.success("Login berhasil")
 
-    menu = st.sidebar.selectbox(
-        "Menu",
-        [
-            "Beranda",
-            "Keranjang",
-            "Checkout",
-            "Riwayat Pesanan",
-            "Admin",
-            "Logout"
-        ]
-    )
+    st.write("Silakan gunakan menu di sidebar")
 
-    if menu == "Beranda":
-
-        home()
-
-    elif menu == "Keranjang":
-
-        cart()
-
-    elif menu == "Checkout":
-
-        checkout()
-
-    elif menu == "Riwayat Pesanan":
-
-        riwayat()
-
-    elif menu == "Admin":
-
-        st.title("⚙️ Admin")
-
-        password_admin = st.text_input(
-            "Masukkan Password Admin",
-            type="password"
-        )
-
-        if password_admin == "admin123":
-
-            admin_produk()
-
-        elif password_admin != "":
-
-            st.error(
-                "Password admin salah"
-            )
-
-    elif menu == "Logout":
-
+    if st.button("Logout"):
         st.session_state.login = False
-        st.session_state.user = ""
-
+        st.session_state.user_id = None
+        st.session_state.role = ""
         st.rerun()
+
